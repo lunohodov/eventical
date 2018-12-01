22854a2e8274110572557d6711665cc403ee0190
Yanko Ivanov <yanko.ivanov@onmoon.org>
2018-12-01 15:59:51 +0100

    Landing page design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :signed_in?
+
+  protected
+
+  def signed_in?
+    params[:logged].presence
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def body_class
+    qualified_controller_name = controller.controller_path.gsub('/','-')
+    "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name}"
+  end
 end
