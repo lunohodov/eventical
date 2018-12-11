07eaf719d228f0cb511afc76e51efa1d8f60cb06
Yanko Ivanov <yanko.ivanov@onmoon.org>
2018-12-11 21:04:40 +0100

    Request SSO scope permissions on login
    The one scope we require ("esi-calendar.read_calendar_events.v1") is not
    going to cause cluttered login screens.
    
    Closes #3

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :eve_online_sso,
     ENV["EVE_ONLINE_CLIENT_ID"],
-    ENV["EVE_ONLINE_SECRET_KEY"]
+    ENV["EVE_ONLINE_SECRET_KEY"],
+    scope: "esi-calendar.read_calendar_events.v1"
 end
