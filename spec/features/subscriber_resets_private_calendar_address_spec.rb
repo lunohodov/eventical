f381cee90d0784da936bca3ce0a697ad6a3693c4
Yanko Ivanov <yanko.ivanov@onmoon.org>
2019-05-24 05:02:30 +0200

    DRY tests

--- spec/features/subscriber_resets_private_calendar_address_spec.rb
+++ spec/features/subscriber_resets_private_calendar_address_spec.rb
@@ -7,20 +7,24 @@ feature "subscriber resets private calendar address", type: :feature do
     initial_token = create(:access_token, :personal, issuer: current_character)
 
     visit calendar_path
-    click_button(id: "btn-reset-private-address")
+    click_reset_button
 
     expect(page).not_to have_link(href: /#{initial_token.token}$/)
   end
 
-  scenario "and can not see reset address" do
+  scenario "and can not access the just reset address" do
     sign_in
 
     initial_token = create(:access_token, :personal, issuer: current_character)
 
     visit calendar_path
-    click_button(id: "btn-reset-private-address")
+    click_reset_button
     visit calendar_feed_path(id: initial_token.token)
 
     expect(page).to have_text(/not found/i)
+  end
+
+  def click_reset_button
+    click_button(id: "btn-reset-private-address")
   end
 end
