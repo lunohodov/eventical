aa53961e17d5876b9dbf01490544fe7848b4e9af
Yanko Ivanov <yanko.ivanov@onmoon.org>
2019-05-30 08:38:43 +0200

    Enable Skylight in staging and dev environments

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module Eventical
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.skylight.environments += %w[development staging]
   end
 end
