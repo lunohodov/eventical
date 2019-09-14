511b18bd1fef3242e9b08777e3f4331360cf2c68
Yanko Ivanov <yanko.ivanov@onmoon.org>
2019-09-14 09:39:50 +0300

    Handle failure to send events to Sentry
    * Retry job exponentially
    * Log failures to `Rails.logger`

--- app/jobs/sentry_job.rb
+++ app/jobs/sentry_job.rb
@@ -1,6 +1,12 @@
 class SentryJob < ApplicationJob
   queue_as :error
 
+  retry_on(StandardError, wait: :exponentially_longer, attempts: 5) do |job, e|
+    Rails.logger.error(
+      "Error logging to Sentry: #{job.arguments.inspect}. Cause: #{e.message}",
+    )
+  end
+
   def perform(event)
     Raven.send_event(event)
   end
