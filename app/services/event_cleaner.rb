f00c429ae57ee27ccacc2457cb0f60d9e8b366d7
Yanko Ivanov <yanko.ivanov@onmoon.org>
2020-08-21 11:50:31 +0300

    Introduce a service to cleanup events

---
+++ app/services/event_cleaner.rb
@@ -0,0 +1,29 @@
+class EventCleaner
+  attr_reader :max_event_age
+
+  def self.call
+    new(max_event_age: 6.months).call
+  end
+
+  def initialize(max_event_age:)
+    @max_event_age = max_event_age
+  end
+
+  def call
+    [
+      clean_aged,
+      clean_belonging_to_deactivated_characters
+    ].sum
+  end
+
+  private
+
+
+  def clean_aged
+    Event.delete_by("created_at < ?", max_event_age.ago)
+  end
+
+  def clean_belonging_to_deactivated_characters
+    Event.delete_by(character: Character.deactivated)
+  end
+end
