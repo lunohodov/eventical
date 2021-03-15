f0917c6d83fde8f04df721e2619b9d7ed8097c96
Yanko Ivanov <yanko.ivanov@onmoon.org>
2021-03-15 10:23:00 +0200

    Refactor EventCleaner

--- spec/services/event_cleaner_spec.rb
+++ spec/services/event_cleaner_spec.rb
@@ -1,41 +1,44 @@
 require "rails_helper"
 
 describe EventCleaner, type: :model do
-  describe ".call" do
-    it "deletes events older than 6 months" do
+  describe "#call" do
+    it "does not delete events happening in the future (minus 1 week)" do
+      create(:event, title: "3 days ago", starts_at: 3.days.ago)
+      create(:event, title: "Today", starts_at: Time.current)
+      create(:event, title: "Next week", starts_at: 1.week.from_now)
+
+      EventCleaner.new.call
+      events_left = Event.pluck(:title)
+
+      expect(events_left).to match_array(["3 days ago", "Today", "Next week"])
+    end
+
+    it "deletes events happened over 1 week ago" do
       create(:event, title: "left")
-      create(:event, title: "older", created_at: 7.months.ago)
+      create(:event, title: "deleted", starts_at: 8.days.ago)
 
-      EventCleaner.call
-      result = Event.all.map(&:title)
+      EventCleaner.new.call
+      events_left = Event.pluck(:title)
 
-      expect(result).to eq(["left"])
+      expect(events_left).to eq(["left"])
     end
 
-    it "does not delete events belonging to recently deactivated characters" do
-      character = create(:character, refresh_token_voided_at: 1.month.ago)
-      create(:event, character: character)
+    it "deletes events owned by voided characters" do
+      character = create(:character, refresh_token_voided_at: Time.current)
+      create(:event, character: character, starts_at: 1.day.from_now)
+      create(:event, character: character, starts_at: Time.current)
 
-      EventCleaner.call
+      EventCleaner.new.call
 
-      expect(character.events).not_to be_empty
-    end
-
-    it "deletes events belonging to characters deactivated 2 months ago" do
-      character = create(:character, refresh_token_voided_at: 2.months.ago)
-      create(:event, character: character)
-
-      EventCleaner.call
-
-      expect(character.events).to be_empty
+      expect(character.reload.events).to be_empty
     end
 
     it "returns the number of deleted events" do
-      create(:event, created_at: 7.months.ago)
+      create(:event, starts_at: 2.weeks.ago)
       create(:event, character: create(:character, refresh_token_voided_at: Time.current))
-      create(:event, character: create(:character, refresh_token_voided_at: 2.months.ago))
+      create(:event)
 
-      result = EventCleaner.call
+      result = EventCleaner.new.call
 
       expect(result).to eq 2
     end
