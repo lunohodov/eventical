4e25f6a276644aae6289e9bf47add0c7271018cb
Yanko Ivanov <yanko.ivanov@onmoon.org>
2021-11-20 14:04:21 +0200

    Refactor how we set Sentry's user context

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,14 +1,6 @@
-require "eventical/middleware/add_character_to_sentry_context"
-
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger]
   config.excluded_exceptions -= ["ActiveRecord::RecordNotFound"]
   config.release = "eventical@#{Eventical.release_version}"
   config.traces_sample_rate = 0.25
-end
-
-module Eventical
-  class Application < Rails::Application
-    config.middleware.use(Middleware::AddCharacterToSentryContext, Sentry)
-  end
 end
