63a3756bbf6b5a77fd7cd2244a7dc265af119a47
Yanko Ivanov <yanko.ivanov@onmoon.org>
2021-11-20 12:26:42 +0200

    Tell Sentry to sample every fifth transaction

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,6 +4,7 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger]
   config.excluded_exceptions -= ["ActiveRecord::RecordNotFound"]
   config.release = "eventical@#{Eventical.release_version}"
+  config.traces_sample_rate = 0.25
 end
 
 module Eventical
