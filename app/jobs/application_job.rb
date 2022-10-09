bf845894ea0e79c72b37ed50e5118fa2630414bd
Yanko Ivanov <yanko.ivanov@onmoon.org>
2022-10-09 08:26:59 +0200

    Remove unused code

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,13 +1,6 @@
 class ApplicationJob < ActiveJob::Base
   delegate :logger, to: Rails
 
-  def report_error(error)
-    Sentry.capture_exception(
-      error,
-      extra: {job: self, arguments: arguments}
-    )
-  end
-
   def analytics
     Analytics.new
   end
